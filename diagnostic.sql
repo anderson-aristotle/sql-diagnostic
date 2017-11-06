4e8db1434375326d6b0272790cab548219c90313
jordanallain <jallain@gmail.com>
2017-11-06 15:34:39 -0500

    Remove line numbers from questions
    Realized that adding answers to the questions will change which line
    numbers they should be adding their answers to. Remove references to
    line numbers and instead tell them to start on the following line.

--- diagnostic.sql
+++ diagnostic.sql
@@ -8,14 +8,14 @@ CREATE DATABASE "sql-diagnostic";
 Question 1. Create a table called `licensees` to hold data in columns last_name, first_name, gender, height, weight, born_on, eye_color, hair_color.  See `data/licensees.csv` for example data.  Don't forget that all tables should have an id column as a unique identifier for a row.
 */
 
--- Your answer to question 1 should start on line 12
+-- Your answer to question 1 should start on the following line
 
 
 /*
 Question 2. Add `Bishop, Caren, f, 63, 132, 1943-09-26, Brown, Black` into the table of licensees.
 */
 
--- Your answer to question 2 should go on line 19
+-- Your answer to question 2 should start on the following line
 
 
 /*
@@ -26,14 +26,14 @@ After completing questions 1 and 2 uncomment the code on line 25 (remove the two
 
 -- Question 3. Write a query to get all attributes of licensees with Hazel eye color and Brown or Black hair color.
 
--- Your answer to question 3 should go on line 30
+-- Your answer to question 3 should start on the following line
 
 
 -- Question 4. Make each of Marilynn Escobar and Chris Whaley one inch shorter. Look them up by their names.
 
--- Your answers to question 4 should start on line 35
+-- Your answers to question 4 should start on the following line
 
 
 -- Question 5. Remove Dylan Rich and Teresita Myers from the table of licensees.
 
--- Your answers to question 5 should start on line 40
+-- Your answers to question 5 should start on the following line
